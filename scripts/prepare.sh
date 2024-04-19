f5dbd2a15417963078b9740e1730a17fd42cc223
znas <31381226+znas-io@users.noreply.github.com>
2024-04-19 00:57:56 +0000

    added task to purge unused packages. now using pipreqs for generating the requirements.txt

--- scripts/prepare.sh
+++ scripts/prepare.sh
@@ -12,5 +12,13 @@ else
     echo "Directory 'diagrams' created successfully."
 fi
 
+# Check if pipreqs is installed and install if it isn't
+if ! pip show pipreqs > /dev/null; then
+    echo "pipreqs not installed. Installing pipreqs..."
+    pip install pipreqs
+else
+    echo "pipreqs is already installed."
+fi
+
 # Install Python dependencies
 pip install -r requirements.txt
