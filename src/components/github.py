f496cd028fe09612fbe4fd072683bf9059e849fd
znas <31381226+znas-io@users.noreply.github.com>
2024-04-23 06:43:05 +0000

    using trio

--- src/components/github.py
+++ src/components/github.py
@@ -1,5 +1,6 @@
 import logging
 import os
+import trio
 
 from . import Agent
 from typing import List
@@ -45,15 +46,18 @@ class Github(Agent):
     def _handle_event(self, event):
         super()._handle_event(event)
         if isinstance(event, IdentifiedProjectStatusMessageEvent):
-            self._handle_identified_project_status_message_event(event)
+            trio.run(self._handle_identified_project_status_message_event, event)
         else:
             logging.debug(f"Event '{type(event).__name__}' is not supported.")
 
-    def _handle_identified_project_status_message_event(self, event: IdentifiedProjectStatusMessageEvent):
-        dataset = _new_config_dataset()
-        for query_item in dataset:
+    async def _handle_identified_project_status_message_event(self, event: IdentifiedProjectStatusMessageEvent):
+        async def process_query_item(event: IdentifiedProjectStatusMessageEvent, query_item: ProjectStatusQueryItem):
             output = self._invoke_prompt(prompt="znas/answer_project_status_message_question",
                                         input={"project": event.project, **query_item.model_dump()})
             query_item.answer = output["output"]
+        dataset = _new_config_dataset()
+        async with trio.open_nursery() as nursery:
+            for query_item in dataset:
+                nursery.start_soon(process_query_item, event, query_item)
         self._emit_event(RespondProjectStatusMessageEvent(**event, dataset=dataset))
         
