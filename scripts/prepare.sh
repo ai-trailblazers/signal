29d2a6b2fa5421c1b0e445ee8391e1afdc3fc596
znas <31381226+znas-io@users.noreply.github.com>
2024-04-24 00:30:49 +0000

    removed pipreqs

--- scripts/prepare.sh
+++ scripts/prepare.sh
@@ -3,13 +3,5 @@
 sudo apt-get update
 sudo apt-get install graphviz -y
 
-# Check if pipreqs is installed and install if it isn't
-if ! pip show pipreqs > /dev/null; then
-    echo "pipreqs not installed. Installing pipreqs..."
-    pip install pipreqs
-else
-    echo "pipreqs is already installed."
-fi
-
 # Install Python dependencies
 pip install -r requirements.txt
